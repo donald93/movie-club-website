1522e58f0f58e3fb1d84b416862eb578f60345b8
Donald McPartland <donald.mcpartland@sprbrk.com>
2022-12-20 14:04:36 -0800

    Update models so admin can create some test data

--- movieclubsite/main/admin.py
+++ movieclubsite/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Session)
+admin.site.register(CategoryPoll)
+admin.site.register(PollResponses)
+admin.site.register(Category)
+admin.site.register(Movie)
--- movieclubsite/main/models.py
+++ movieclubsite/main/models.py
@@ -5,31 +5,44 @@ from django.contrib.auth.models import User
 
 class Session(models.Model):
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return "Session" + self.pk
 
 class CategoryPoll(models.Model):
     open_date = models.DateField()
-    close_date = models.DateField()
+    close_date = models.DateField(null=True, blank=True)
     session = models.OneToOneField(Session, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Session" + self.session.pk + " poll"
 
 class PollResponses(models.Model):
     category_poll = models.ForeignKey(CategoryPoll, on_delete=models.CASCADE)
     submitter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     response = models.CharField(max_length=2550)
 
+    def __str__(self):
+        return "Poll Response Submitted By " + self.submitter + " For Poll " + self.category_poll.pk
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=2000)
+    description = models.CharField(max_length=2000, null=True, blank=True)
     submitter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     selected = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name + " " + self.description
+ 
 class Movie(models.Model):
     name = models.CharField(max_length=255)
-    release_year = models.IntegerField()
+    release_year = models.IntegerField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     submitter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     multiplier = models.IntegerField()
     watched = models.BooleanField(default=False)
 
-
+    def __str__(self):
+        return self.name
